97c9232b25b4298eef3a2e7502092b3731d90532
lamamaflores <lamamaflores@gmail.com>
2023-10-19 11:20:10 -0400

    learned data types, variables, conditional stmts

---
+++ Basic.py
@@ -0,0 +1,17 @@
+print("Kristin")
+x=input("give me an odd number")
+print(x)
+# DataTypes
+# 1. whole numbers without decimal point= int
+# 2. numbers which contain decimal points= float
+# 3. stores combination of characters (numbers, symbols, letters etc)= strings
+# 4. only one single character=char
+# 5. stores true or false= bool
+
+# Variable Naming Convention
+# 1. can use upper and lower case alphabet anywhere in the variable
+# 2. cannot start a variable with a number NEVER! can be anywhere else
+# 3. cannot use a space, or multiple spaces in between our variable names
+# 4. only underscore can be used. No other symbols can be used in the variable name
+# 5. we cannot use reserved keywords as a variable name. Cannot be exact name as reserved keyword but can be a variation
+
---
+++ Conditional_statements.py
@@ -0,0 +1,111 @@
+# if 3 == 4:
+#     print("Equal")
+# elif (3 > 4):
+#     print("greater")
+# else:
+#     print("less")
+
+# if 13 > 4:
+#     print("greater")
+# elif 13 < 4:
+#     print("less")
+# else:
+#     print("equal")
+
+# x = 4 
+# if x == 0:
+#     print("equal")
+# elif x >= 4:
+#     print("greater or equal")
+# elif x > 4:
+#     print("greater")
+# else:
+#     print("default")
+
+# x = 3 
+# if x == 0:
+#     print("equal")
+# elif x >= 4:
+#     print("greater or equal")
+# elif x > 4:
+#     print("greater")
+# else:
+#     print("default")
+
+# if 3 > 4 or 5 > 4:
+#     print("one")
+# elif 8 > 4 and 6 > 4:
+#     print("two")
+# else:
+#     print("none")
+
+# if 3 > 4 or 5 < 4:
+#     print("one")
+# elif 8 > 4 and 6 > 4:
+#     print("two")
+# else:
+#     print("none")
+
+# if not(5 < 4):
+#     print("one")
+# elif 4 > 3:
+#     print("two")
+# else:
+#     print("none")
+
+# if not(4 > 6) and not(8 > 5):
+#     print("one")
+# elif 4 > 5 or not(5 == 5):
+#     print("two")
+# else:
+#     print("none")
+
+# if 4 < 3:
+#     print("three")
+# if 4 > 2:
+#     print("four")
+# if 5 < 4:
+#     print("five")
+# elif 4 != 4:
+#     print("six")
+# elif 4 > 3:
+#     print("seven")
+# else:
+#     print("default")
+
+# x = 3
+# y = 12
+# if x <= y and x > 0:
+#     print("three")
+# if x > y or y < 11:
+#     print("four")
+# if not(x < 4) and y < 24:
+#     print("five")
+# elif y != 4:
+#     print("six")
+# elif y >= x:
+#     print("seven")
+# else:
+#     print("default")
+
+x = 12
+y = 3
+z = 11
+if (x == 2 and x < 3):
+    print(x)
+if x != 5:
+    print("whatever")
+if x != 5 and y >= 5 and z <= 13:
+    print("its gonna happen")
+if z != 0 or x == 2:
+    print("you say")
+if (not(y < 10)):
+    print(y)
+elif(x < 10 or x < 5):
+    print("okkkkkkk")
+elif(y < 10 or y <= 0):
+    print("pikachu")
+elif(z == 0 or y ==5):
+    print("Pikaboo")
+else:
+    print("Default value")
